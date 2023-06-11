69b539e0bf8a73f96fb8255a8dbec16e85450147
LiK-h1n <likh1n709@gmail.com>
2023-06-11 14:09:12 -0700

    Detecting objects when exiting frame

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from cv2 import VideoCapture, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, GaussianBlur, absdiff, threshold, \
     THRESH_BINARY, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, contourArea, boundingRect, rectangle
 from time import sleep
+from emailing import send_email
 
 video = VideoCapture(0)
 sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_scale = cvtColor(frame, COLOR_BGR2GRAY)
     gray_scale_gau = GaussianBlur(gray_scale, (21, 21), 0)
@@ -26,7 +29,15 @@ while True:
         if contourArea(contour) < 5000:
             continue
         x, y, w, h = boundingRect(contour)
-        rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rect = rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rect.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
 
     imshow("Video", frame)
 
