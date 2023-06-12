e0c7124566b2a29f38df052ee54c231bf51e4da7
LiK-h1n <likh1n709@gmail.com>
2023-06-12 13:00:02 -0700

    Extracting images from webcam

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from cv2 import VideoCapture, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, GaussianBlur, absdiff, threshold, \
-    THRESH_BINARY, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, contourArea, boundingRect, rectangle
+    THRESH_BINARY, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, contourArea, boundingRect, rectangle, \
+    imwrite
 from time import sleep
+from glob import glob
+from os.path import getmtime, join
+from os import listdir, remove
 from emailing import send_email
 
+
+def delete_old_images():
+    directory_path = "images"
+
+    for image in listdir(directory_path):
+        image_path = join(directory_path, image)
+        remove(image_path)
+
+
+delete_old_images()
 video = VideoCapture(0)
 sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -32,12 +47,17 @@ while True:
         rect = rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rect.any():
             status = 1
+            imwrite(f"images/{count}.png", frame)
+            count += 1
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        all_images = sorted(glob("images/*.png"), key=getmtime)
+        index = int(len(all_images) / 2)
+        image_with_object = all_images[index]
+        # send_email(image_with_object)
 
     imshow("Video", frame)
 
